3297ac781ba2a1f0ae3e9c736dd3d9ce58913003
MargaritaMalakhova <ritulek@mail.ru>
2022-08-11 17:16:29 +0200

    add new test for task 6 (with stop browser)

--- java-example/src/test/java/MyThirdTest.java
+++ java-example/src/test/java/MyThirdTest.java
@@ -66,8 +66,8 @@ public class MyThirdTest {
     @AfterEach
     public void stop()
     {
-        //driver.quit();
-        //driver = null;
+        driver.quit();
+        driver = null;
     }
 }
 
