fcef7ff8a4e6779c710a34d3c9c6425780a04425
MargaritaMalakhova <ritulek@mail.ru>
2022-08-25 13:11:48 +0200

    add img for task 13

---
+++ java-example/src/test/java/MytwelfthTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+
+public class MytwelfthTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void mytwelfthTest() {
+        List<WebElement> lastestProducts = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            driver.navigate().to("http://localhost/litecart/");
+            wait.until(titleIs("Online Store | My Store"));
+            lastestProducts = driver.findElements(By.cssSelector("#box-latest-products > div > ul > li > a.link"));
+            lastestProducts.get(i).click();
+            WebElement amountItemsOld = driver.findElement(By.cssSelector("#cart > a.content > span.quantity"));
+            WebElement buttonAdd = driver.findElement(By.name("add_cart_product"));
+            wait.until(visibilityOf(buttonAdd));
+            buttonAdd.click();
+            String spanText = Integer.toString(i + 1);
+            wait.until(attributeContains(amountItemsOld, "textContent", spanText));
+        }
+
+        driver.navigate().to("http://localhost/litecart/");
+        wait.until(titleIs("Online Store | My Store"));
+        driver.findElement(By.cssSelector("#cart a.link")).click();
+        wait = new WebDriverWait(driver, 10);
+
+        for (int i = 0; i < 2; i++) {
+            driver.findElement(By.cssSelector("#box-checkout-cart li:nth-child(1) > a")).click();
+            WebElement buttonRemove = driver.findElement(By.name("remove_cart_item"));
+            WebElement removableElementInTable = driver.findElement(By.cssSelector("table > tbody > tr:nth-child(2) > td.item"));
+            buttonRemove.click();
+            wait.until(ExpectedConditions.stalenessOf(removableElementInTable));
+        }
+        WebElement buttonRemove = driver.findElement(By.name("remove_cart_item"));
+        WebElement removableElementInTable = driver.findElement(By.cssSelector("table > tbody > tr:nth-child(2) > td.item"));
+        buttonRemove.click();
+        wait.until(ExpectedConditions.stalenessOf(removableElementInTable));
+
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
